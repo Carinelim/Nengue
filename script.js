313700a817bf31c0161fabd8e01f245cc434e35c
Carine Lima <carine1.silva14@gmail.com>
2024-07-16 20:48:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Hello nengue!!")
+var respost = prompt("Como você está hoje?")
+console.log("Resposta como você está hoje" + respost)
+alert("Espero que você esteja bem! :)")
+prompt("Se hidratou direitinho hoje?")
+
+console.log(confirm("Tem certeza que você se hidratou hoje direitinho?"))
+
+let resposta = confirm("Tem certeza absoluta???")
+if(resposta == true){
+    alert("Que ótimo nengue!")
+} else{ 
+    alert("Se não se hidratou, vai cair no barrote!")
+}
+alert("Tô de olho!")
+
+alert("Se cuide direitinho!")
